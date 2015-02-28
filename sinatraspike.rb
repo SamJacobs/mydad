6de16f5a8be09dc3a9dcb66e267ac16995c33862
Samuel Jacobs <samueljacobs@Samuels-MacBook-Air.local>
2015-02-28 14:18:49 -0600

    styling

--- sinatraspike.rb
+++ sinatraspike.rb
@@ -4,6 +4,7 @@ require 'uri'
 require 'rest-client'
 require 'base64'
 require 'twitter'
+require 'htmlentities'
 
 require_relative 'info'
 
@@ -11,6 +12,14 @@ $client = Twitter::REST::Client.new do |config|
   config.consumer_key        = $consumer_key
   config.consumer_secret     =  $consumer_secret
 end
+
+encoding_options = {
+    :invalid           => :replace,  # Replace invalid byte sequences
+    :undef             => :replace,  # Replace anything not defined in ASCII
+    :replace           => '',        # Use a blank for those replacements
+    :universal_newline => true       # Always break lines with \n
+  }
+
 
 $auth_method        = :oauth
 
@@ -21,7 +30,14 @@ end
 get '/more' do
 	$client.search("'my dad'", result_type: "recent").each do |tweet|
 		if tweet.text =~ /\Amy\sdad\s/i
-    		return tweet.text[7...tweet.text.length]
+			tweet = HTMLEntities.new.decode tweet.text[7...tweet.text.length]
+			tweet = tweet.encode Encoding.find('ASCII'), encoding_options
+			if not (tweet.match /\bhttp/i).nil?
+				nextwordArray = tweet.split(' ')
+				nextwordArray.map! {|word| word[0..3] == "http" ? "<a class=\"link\" href=\"#{word}\">#{word}</a>" : word } 
+				tweet = nextwordArray.join(' ')  
+			end   
+    		return tweet
     	end
 	end
 end
